b92a191aef8f26b80f893eb5b30d2a6b4cd7d1ae
Lionad <tangnad@qq.com>
2020-08-03 17:00:09 +0800

    chore: rollup copy plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,24 @@
 const path = require('path')
 
+const shouldRun = !!process.env.RUN
 const inputPath = process.env.INPUT
 const inputFileDir = path.dirname(inputPath)
-
-console.log(inputPath, inputFileDir)
+const outputDir = './build/'
 
 export default {
   input: inputPath,
   plugins: [
+    require('rollup-plugin-copy')({
+      targets: [{ src: inputFileDir + '/**/*', dest: `${outputDir}${inputFileDir}` }],
+    }),
     require('rollup-plugin-commonjs')(),
     require('rollup-plugin-node-resolve')(),
-    require('@rollup/plugin-run')(),
+    shouldRun && require('@rollup/plugin-run')(),
   ],
   external: [/node_modules/],
   output: {
     name: 'flowchart',
-    file: `./build/${inputPath}`,
+    file: `${outputDir}${inputPath}`,
     format: 'umd',
     sourcemap: true,
   },
