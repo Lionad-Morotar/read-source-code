17323b4ac857110676568771eabfe1b2d0d18e09
Lionad <tangnad@qq.com>
2020-08-03 17:15:53 +0800

    chore: rollup dev-server

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,32 @@
 const path = require('path')
 
 const shouldRun = !!process.env.RUN
+const openDevServer = !!process.env.serve
 const inputPath = process.env.INPUT
 const inputFileDir = path.dirname(inputPath)
 const outputDir = './build/'
+const outputDest = `${outputDir}${inputFileDir}`
 
 export default {
   input: inputPath,
   plugins: [
     require('rollup-plugin-copy')({
-      targets: [{ src: inputFileDir + '/**/*', dest: `${outputDir}${inputFileDir}` }],
+      targets: [{ src: inputFileDir + '/**/*', dest: outputDest }],
     }),
     require('rollup-plugin-commonjs')(),
     require('rollup-plugin-node-resolve')(),
     shouldRun && require('@rollup/plugin-run')(),
+    openDevServer &&
+      require('rollup-plugin-serve')({
+        /** @see https://www.npmjs.com/package/rollup-plugin-serve */
+        verbose: false,
+        host: 'localhost',
+        port: 10001,
+        contentBase: outputDest,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      }),
   ],
   external: [/node_modules/],
   output: {
