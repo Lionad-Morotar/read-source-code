621cdb13d55c7a91ba044470ad5de77d93e11ba7
Lionad <tangnad@qq.com>
2020-08-10 15:26:02 +0800

    refactor: promise polyfill

--- polyfill/promise.js
+++ polyfill/promise.js
@@ -1,47 +1,89 @@
-function Promise (fns) {
-  this.state = 'initial'
+const PENDING = 'pending'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
+const microTimeout = (process && process.nextTick) || queueMicrotask || (cb => setTimeout(cb, 0))
+
+function Promise(fns) {
+  this.state = PENDING
   this.thenFns = []
+  this.rejectFns = []
+
   const promise = new Proxy(this, {
-    get (target, prop) {
+    get(target, prop) {
       if (prop === 'then') {
-        return then
+        return thenable
+      }
+      if (prop === 'catch') {
+        return catchable
       }
       return target[prop]
-    }
+    },
   })
 
-  function fulfilled () {
-    this.state = 'fulfilled'
+  function thenable(cb) {
+    if (promise.state === PENDING) {
+      this.thenFns.push(cb)
+      return promise
+    }
+    throw Error('Promise has Done')
   }
 
-  function then (cb) {
-    this.thenFns.push(cb)
-    return promise
+  function catchable(cb) {
+    if (promise.state === PENDING) {
+      this.rejectFns.push(cb)
+      return promise
+    }
   }
-  function resolver (data) {
-    fulfilled()
-    const microTimeout = process && process.nextTick || queueMicrotask || (cb => setTimeout(cb, 0))
-    microTimeout(() => {
-      promise.thenFns.reduce((h, c) => c(h), data)
+
+  function resolve(data) {
+    return microTimeout(() => {
+      if (promise.state === PENDING) {
+        promise.state = FULFILLED
+        microTimeout(() => {
+          try {
+            promise.thenFns.reduce((h, c) => c(h), data)
+          } catch (err) {
+            reject(err)
+          }
+        })
+        return promise
+      }
     })
   }
 
-  fns(resolver)
+  function reject(err) {
+    return microTimeout(() => {
+      if ([PENDING, FULFILLED].includes(promise.state)) {
+        promise.state = REJECTED
+        microTimeout(() => {
+          promise.rejectFns.reduce((h, c) => c(h), err)
+        })
+        return promise
+      }
+    })
+  }
+
+  try {
+    fns(resolve, reject)
+  } catch (err) {
+    reject(err)
+  }
+
   return promise
 }
 
 new Promise(resolve => {
   console.log(1)
   resolve(2)
-}).then(data => {
-  console.log(data)
-  return 3
-}).then(data => {
-  console.log(data)
-  return 4
 })
-
-// >>> 1
-// >>> 2
-// >>> 3
-// 这玩意儿我写出来为什么这么短... 总之肯定有 n 多测试跑不通...
+  .then(data => {
+    console.log(data)
+    return 3
+  })
+  .then(data => {
+    console.log(data)
+    return error
+  })
+  .catch(error => {
+    console.log('[ERR]', error)
+  })
