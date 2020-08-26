106b4944cb83a174d11dc9a75716dd69a7c80fbf
Lionad <tangnad@qq.com>
2020-08-26 21:56:37 +0800

    feat: 递归版本的快排

--- algorithm/quicksort.js
+++ algorithm/quicksort.js
@@ -1,9 +1,29 @@
-function qsort (arrs, left, right) {
+// 递归版本（非常慢）
+const qsort_recurv = array => {
+  if (array.length <= 1) {
+    return array
+  }
+  const [first, ...rest] = array
+  const smaller = [],
+    bigger = []
+  for (var i = 0; i < rest.length; i++) {
+    const value = rest[i]
+    value < first ? smaller.push(value) : bigger.push(value)
+  }
+  return [...quickSort(smaller), first, ...quickSort(bigger)]
+}
+
+function qsort(arrs, left, right) {
   const base = arrs[left]
-  let i = left; let j = right
+  let i = left
+  let j = right
   while (i !== j) {
-    while (base <= arrs[j] && i < j) { j-- }
-    while (base >= arrs[i] && i < j) { i++ }
+    while (base <= arrs[j] && i < j) {
+      j--
+    }
+    while (base >= arrs[i] && i < j) {
+      i++
+    }
     if (i < j) {
       const temp = arrs[i]
       arrs[i] = arrs[j]
