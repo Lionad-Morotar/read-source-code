fdeb8a7422be63a61de7625f357b01a42f40b22f
仿生狮子 <tangnad@qq.com>
2022-05-22 19:42:31 +0800

    feat: double linked list node

--- data-structure/node.js
+++ data-structure/node.js
@@ -1,13 +1,25 @@
 /** Node Constructor */
 
+const getRandID = () => `${+new Date()}_${String(Math.random()).slice(-6)}`
+
+/**
+ * Double Linked List Node
+ */
 function Node(config) {
-  this.key = config.key || String(Math.random()).slice(-5)
+  if (config && config.data) {
+    this.key = config.key || getRandID()
+    this.data = config.data || null
+  } else {
+    this.key = getRandID()
+    this.data = config
+  }
   this.prev = null
   this.next = null
-  this.data = config.data || null
 }
 
 /** Node prototype */
+
+Node.prototype[Symbol.toStringTag] = 'double-linked-list-node'
 
 // 获取节点数据
 Node.prototype.get = function (prop) {
@@ -16,38 +28,82 @@ Node.prototype.get = function (prop) {
 
 // 设置节点数据
 Node.prototype.set = function (prop, value) {
-  this.data = this.data || {}
-  this.data[prop] = value
-  return this.get(prop)
+  if (value) {
+    this.data = this.data || {}
+    this.data[prop] = value
+    return this.data[prop]
+  } else {
+    this.data = prop
+    return this.data
+  }
 }
 
-// 将当前节点的next指向另一节点 linkNextTo
+// 将当前节点的next指向另一节点
 Node.prototype.linkNext = function (nextNode) {
   this.next = nextNode
   nextNode.prev = this
+}
+Node.prototype.addNexts = function (nextNode) {
+  this.next = this.next ? [...this.next, nextNode] : [nextNode]
+  nextNode.prev = nextNode.prev ? [...nextNode.prev, this] : [this]
 }
 
 Node.prototype.linkPrev = function (prevNode) {
   prevNode.linkNext(this)
 }
+Node.prototype.addPrevs = function (prevNode) {
+  prevNode.addNexts(this)
+}
 
 // 将某节点之后插入当前节点
-Node.prototype.insertAfter = function (prevNode) {
-  const prevNextNode = prevNode.next
-  prevNode.linkNext(this)
-  this.linkNext(prevNextNode)
+Node.prototype.insertAfter = function (targetNode) {
+  const targetNextNode = targetNode.next
+  targetNode.linkNext(this)
+  targetNextNode && this.linkNext(targetNextNode)
 }
+// TODO addAfter
+
+Node.prototype.insertBefore = function (targetNode) {
+  const targetPrevNode = targetNode.prev
+  targetPrevNode && targetPrevNode.linkNext(this)
+  this.linkNext(targetNode)
+}
+// TODO addBefore
 
 // 节点脱链
 Node.prototype.unLink = function () {
   const prev = this.prev
   const next = this.next
+  const isSingleLinkNode = (prev instanceof Node) || (next instanceof Node)
 
-  if (!prev || !next) {
-    // 头节点和尾节点不能 unlink
-    throw new Error('cannot unlink HEAD or TAIL')
+  if (isSingleLinkNode) {
+    if (prev && next) {
+      prev.linkNext(next)
+    }
+    else if (prev) {
+      prev.next = null
+    }
+    else if (next) {
+      next.prev = null
+    }
+  } else {
+    let idx
+    if (prev && next) {
+      idx = prev.next.findIndex(this)
+      prev.next.splice(0, idx, 1)
+      idx = next.prev.findIndex(this)
+      next.prev.splice(0, idx, 1)
+      prev.addNexts(next)
+    }
+    else if (prev) {
+      idx = prev.next.findIndex(this)
+      prev.next.splice(0, idx, 1)
+    }
+    else if (next) {
+      idx = next.prev.findIndex(this)
+      next.prev.splice(0, idx, 1)
+    }
   }
-  prev.linkNext(next)
 }
 
 export default Node
